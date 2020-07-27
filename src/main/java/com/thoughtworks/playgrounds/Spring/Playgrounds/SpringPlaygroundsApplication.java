ead75d445b241fcef19dd3d6840513de3e5c3c20
chenmli2020 <35786460+chenmli@users.noreply.github.com>
2020-07-27 09:59:37 -0500

    First Updates

---
+++ src/main/java/com/thoughtworks/playgrounds/Spring/Playgrounds/SpringPlaygroundsApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.playgrounds.Spring.Playgrounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlaygroundsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/playgrounds/Spring/Playgrounds/SpringPlaygroundsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.playgrounds.Spring.Playgrounds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaygroundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
